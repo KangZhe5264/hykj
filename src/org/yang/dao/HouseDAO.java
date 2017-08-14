6da4d48f44610e47c1bc94e8084dc7b7446f053f
杨殊缘 <3241105051@qq.com>
2017-08-14 10:55:41 +0800

    小修改01

--- src/org/yang/dao/HouseDAO.java
+++ src/org/yang/dao/HouseDAO.java
@@ -2,7 +2,6 @@ package org.yang.dao;
 
 import java.util.List;
 
-import org.hibernate.criterion.DetachedCriteria;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Repository;
